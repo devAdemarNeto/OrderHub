4b24e8f556c643f9d53d6ff3ed7af571ddef6f0f
Ademar Neto <ademarnetodev@gmail.com>
2026-01-07 20:30:56 -0300

    chore: initial project setup with Spring Boot

---
+++ src/test/java/dev/ademarneto/OrderHub/OrderHubApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.ademarneto.OrderHub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
