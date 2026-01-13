cfb47acc0e477126355683d5b718c4e0dfa503ce
Ademar Neto <ademarnetodev@gmail.com>
2026-01-12 23:15:43 -0300

    feat: Criada logica para listar pedidos por id no service

--- src/main/java/dev/ademarneto/OrderHub/Service/OrderService.java
+++ src/main/java/dev/ademarneto/OrderHub/Service/OrderService.java
@@ -5,6 +5,7 @@ import dev.ademarneto.OrderHub.repository.OrderRepository;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class OrderService {
@@ -18,6 +19,12 @@ public class OrderService {
     //Listar pedidos
     public List<OrderModel>  listarPedidos(){
         return orderRepository.findAll();
+    }
+
+    //Listar pedidos por id
+    public OrderModel listarPedidosId(Long id){
+        Optional<OrderModel> orderID = orderRepository.findById(id);
+        return orderID.orElse(null);
     }
 
 
