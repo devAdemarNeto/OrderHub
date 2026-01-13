efdb2f3fdafebb7c647e8ae5ac5694f3dcca1b61
Ademar Neto <ademarnetodev@gmail.com>
2026-01-13 20:37:07 -0300

    feat: Criada logica para listar clientes por id no service

--- src/main/java/dev/ademarneto/OrderHub/Service/ClientService.java
+++ src/main/java/dev/ademarneto/OrderHub/Service/ClientService.java
@@ -1,9 +1,11 @@
 package dev.ademarneto.OrderHub.Service;
 
 import dev.ademarneto.OrderHub.model.ClientModel;
+import dev.ademarneto.OrderHub.model.OrderModel;
 import dev.ademarneto.OrderHub.repository.ClientRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public class ClientService {
     private ClientRepository clientRepository;
@@ -15,6 +17,12 @@ public class ClientService {
     //Listar Clientes
     public List<ClientModel> listarClientes(){
         return clientRepository.findAll();
+    }
+
+    //Listar Clientes por Id
+    public ClientModel listarClientesID(Long id){
+        Optional<ClientModel> clientID = clientRepository.findById(id);
+        return clientID.orElse(null);
     }
 
 
--- src/main/java/dev/ademarneto/OrderHub/Service/OrderService.java
+++ src/main/java/dev/ademarneto/OrderHub/Service/OrderService.java
@@ -27,6 +27,4 @@ public class OrderService {
         return orderID.orElse(null);
     }
 
-
-
 }
