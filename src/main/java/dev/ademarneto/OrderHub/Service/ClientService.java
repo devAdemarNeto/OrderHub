a41681df8ab8716fcf089c362df569d4e08a0ec8
Ademar Neto <ademarnetodev@gmail.com>
2026-01-12 22:53:09 -0300

    feat: Injeta a dependencia do service e do repository e cria a lógica para listar os clientes e pedidos

--- src/main/java/dev/ademarneto/OrderHub/Service/ClientService.java
+++ src/main/java/dev/ademarneto/OrderHub/Service/ClientService.java
@@ -1,4 +1,23 @@
 package dev.ademarneto.OrderHub.Service;
 
+import dev.ademarneto.OrderHub.model.ClientModel;
+import dev.ademarneto.OrderHub.repository.ClientRepository;
+
+import java.util.List;
+
 public class ClientService {
+    private ClientRepository clientRepository;
+
+    public ClientService(ClientRepository clientRepository) {
+        this.clientRepository = clientRepository;
+    }
+
+    //Listar Clientes
+    public List<ClientModel> listarClientes(){
+        return clientRepository.findAll();
+    }
+
+
+
+
 }
--- src/main/java/dev/ademarneto/OrderHub/Service/OrderService.java
+++ src/main/java/dev/ademarneto/OrderHub/Service/OrderService.java
@@ -1,4 +1,25 @@
 package dev.ademarneto.OrderHub.Service;
 
-public class OrdeService {
+import dev.ademarneto.OrderHub.model.OrderModel;
+import dev.ademarneto.OrderHub.repository.OrderRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class OrderService {
+    private OrderRepository orderRepository;
+
+    public OrderService(OrderRepository orderRepository) {
+        this.orderRepository = orderRepository;
+    }
+
+
+    //Listar pedidos
+    public List<OrderModel>  listarPedidos(){
+        return orderRepository.findAll();
+    }
+
+
+
 }
